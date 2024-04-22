a063b899456235a2395c1519664be3d4be037cbf
Edgard Hubner <68572500+edhubner@users.noreply.github.com>
2024-04-22 12:24:00 +0000

    Exercicio 04_TAD_simples/TAD_00

---
+++ 04_TAD_simples/TAD_00/Resultados/EdgardHubner/main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "ponto.h"
+
+int main(void)
+{
+    float x1, y1, x2, y2;
+    scanf("%f %f %f %f\n", &x1, &y1, &x2, &y2);
+
+    Ponto p1 = pto_cria(x1, y1);
+    Ponto p2 = pto_cria(x2, y2);
+
+    printf("%g", pto_distancia(p1, p2));
+
+    return 0;
+}
